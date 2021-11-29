0d760e0e21fa44bbac8ce28a53e2171032b0dc86
Papakwesi <93794323+Papakwesi@users.noreply.github.com>
2021-11-29 10:08:49 -0800

    Add lazyresearcher html, css & js

---
+++ code.js
@@ -0,0 +1,6 @@
+// var name = window.prompt("Enter your name: ")
+// alert("Hello user this is just to make things easy for you. Please make further research. Thank you.")
+
+function handleClick(element){
+	alert("clicked on " + element.id);
+}
